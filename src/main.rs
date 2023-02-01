7b78a71263d90da5ebbd603fb61dd65653b47012
Folafunmi <folafunmimustapha@gmail.com>
2023-02-01 16:00:41 +0100

    feat: add read from file function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::error::Error;
+
+use csv;
+
+fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::Reader::from_path(path)?;
+    for result in reader.records() {
+        let a = result?;
+        println!("{:?}", a);
+    }
+    // reader.records().into_iter().map(|x| {
+    //     let a = x?;
+    //     println!("{:?}", a);
+    // });
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = read_from_file("./file.csv") {
+        eprintln!("{}", e);
+    }
 }
